a1ce4f597c7915dd28a253e5b3e094f6d6d55ce7
joe <joe@gironsec.com>
2020-04-17 23:36:56 -0700

    new stuff added, codes and the like

---
+++ Wan Thingy/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Wan_Thingy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbHostList = new System.Windows.Forms.TextBox();
+            this.bg = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbExcludeList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTimeOut = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbUA = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClearURL = new System.Windows.Forms.Button();
+            this.btnClearFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(312, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbHostList
+            // 
+            this.tbHostList.Location = new System.Drawing.Point(12, 27);
+            this.tbHostList.MaxLength = 647670;
+            this.tbHostList.Multiline = true;
+            this.tbHostList.Name = "tbHostList";
+            this.tbHostList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbHostList.Size = new System.Drawing.Size(291, 174);
+            this.tbHostList.TabIndex = 2;
+           
+            // 
+            // bg
+            // 
+            this.bg.WorkerReportsProgress = true;
+            this.bg.WorkerSupportsCancellation = true;
+            this.bg.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bg_DoWork);
+            this.bg.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bg_ProgressChanged);
+            this.bg.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bg_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(444, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(312, 118);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(207, 55);
+            this.pb.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(362, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status: Not Running";
+            // 
+            // tbExcludeList
+            // 
+            this.tbExcludeList.Location = new System.Drawing.Point(12, 251);
+            this.tbExcludeList.Multiline = true;
+            this.tbExcludeList.Name = "tbExcludeList";
+            this.tbExcludeList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbExcludeList.Size = new System.Drawing.Size(291, 125);
+            this.tbExcludeList.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Exclude These Strings (one per line)...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "URL List To Scan (one per line)...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Timeout:";
+            // 
+            // tbTimeOut
+            // 
+            this.tbTimeOut.Location = new System.Drawing.Point(379, 87);
+            this.tbTimeOut.Name = "tbTimeOut";
+            this.tbTimeOut.Size = new System.Drawing.Size(39, 20);
+            this.tbTimeOut.TabIndex = 9;
+            this.tbTimeOut.Text = "5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "User Agent:";
+            // 
+            // lbUA
+            // 
+            this.lbUA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbUA.FormattingEnabled = true;
+            this.lbUA.Items.AddRange(new object[] {
+            "Google Bot",
+            "Bing Bot",
+            "DuckDuckBot",
+            "Baidu Bot",
+            "Yandex Bot",
+            "Internet Explorer",
+            "Firefox",
+            "Chrome",
+            "America Online"});
+            this.lbUA.Location = new System.Drawing.Point(379, 27);
+            this.lbUA.Name = "lbUA";
+            this.lbUA.Size = new System.Drawing.Size(132, 54);
+            this.lbUA.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Wan_Thingy.Properties.Resources._1558365013674;
+            this.pictureBox1.Location = new System.Drawing.Point(309, 192);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(424, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "(in seconds)";
+            // 
+            // btnClearURL
+            // 
+            this.btnClearURL.Location = new System.Drawing.Point(12, 207);
+            this.btnClearURL.Name = "btnClearURL";
+            this.btnClearURL.Size = new System.Drawing.Size(75, 25);
+            this.btnClearURL.TabIndex = 14;
+            this.btnClearURL.Text = "Clear List";
+            this.btnClearURL.UseVisualStyleBackColor = true;
+            this.btnClearURL.Click += new System.EventHandler(this.btnClearURL_Click);
+            // 
+            // btnClearFilter
+            // 
+            this.btnClearFilter.Location = new System.Drawing.Point(12, 382);
+            this.btnClearFilter.Name = "btnClearFilter";
+            this.btnClearFilter.Size = new System.Drawing.Size(75, 25);
+            this.btnClearFilter.TabIndex = 14;
+            this.btnClearFilter.Text = "Clear Filters";
+            this.btnClearFilter.UseVisualStyleBackColor = true;
+            this.btnClearFilter.Click += new System.EventHandler(this.btnClearFilter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 414);
+            this.Controls.Add(this.btnClearFilter);
+            this.Controls.Add(this.btnClearURL);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbUA);
+            this.Controls.Add(this.tbTimeOut);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbExcludeList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.tbHostList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HTTP Range Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbHostList;
+        private System.ComponentModel.BackgroundWorker bg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbExcludeList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTimeOut;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbUA;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClearURL;
+        private System.Windows.Forms.Button btnClearFilter;
+    }
+}
+
